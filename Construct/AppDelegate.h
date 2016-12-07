e946ab11a0f1dbbce261c4262ac2de29114b8108
Lisa <1085983505@qq.com>
2016-12-07 18:08:38 +0800

    Initial Commit

---
+++ Construct/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Construct
+//
+//  Created by lisa on 2016/12/7.
+//  Copyright © 2016年 lisa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
